397a42228a648ebd7c08cff76175f564b6ece01b
Luzog78 <luzog78@gmail.com>
2023-11-17 13:49:07 +0100

    Finished ?

--- client.c
+++ client.c
@@ -6,11 +6,63 @@
 /*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 06:30:36 by ysabik            #+#    #+#             */
-/*   Updated: 2023/11/17 06:31:48 by ysabik           ###   ########.fr       */
+/*   Updated: 2023/11/17 09:06:31 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include <unistd.h>
+#include <signal.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_uatoi(const char *str);
+
+int	received;
+
+static void	action(int sig)
 {
+	(void) sig;
+	received = 1;
+}
+
+static void	send(int pid, const char *str)
+{
+	int				sent;
+	unsigned char	curr;
+
+	while(*str)
+	{
+		curr = *str;
+		sent = 0;
+		while (sent < 8)
+		{
+			if (curr & (1 << (7 - sent)))
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			sent++;
+			usleep(100);
+		}
+		str++;
+	}
+	sent = 0;
+	while (sent < 8)
+	{
+		kill(pid, SIGUSR1);
+		sent++;
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	
+	if (argc < 3)
+		return (1);
+	pid = ft_uatoi(argv[1]);
+	signal(SIGUSR1, action);
+	send(pid, argv[2]);
+	while (!received)
+		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,16 +6,64 @@
 /*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 06:30:36 by ysabik            #+#    #+#             */
-/*   Updated: 2023/11/17 06:46:47 by ysabik           ###   ########.fr       */
+/*   Updated: 2023/11/17 09:02:20 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
 #include <unistd.h>
-#include "ft_printf.h"
+#include <signal.h>
+
+int	ft_printf(const char *format, ...);
+
+/**
+ * An int is 4*4 bits long,
+ * A char is only 1*4 bits long.
+ * ==> `data` will use the first 3*8 bits to store the amount of received bits
+ * ==> `data` will use the last 1*8 bits to store the received bit
+ * 
+ * --> 0000 0000  0000 0000  0000 0000  1111 1111 <--
+ *    |    Amount of received bits    ||  Char   |
+*/
+unsigned int	data;
+
+static void	action(int sig, siginfo_t *info, void *context)
+{
+	unsigned int	tmp;
+
+	(void) context;
+	if (info->si_pid == 0)
+		return ;
+	tmp = data % 256;
+	tmp <<= 1;
+	if (sig == SIGUSR2)
+		tmp += 1;
+	data = (data >> 8) + 1;
+	if (data >= 8)
+	{
+		if (tmp)
+			write(1, &(char){ tmp }, 1);
+		else
+		{
+			write(1, "\n", 1);
+			kill(info->si_pid, SIGUSR1);
+		}
+		data = 0;
+	}
+	else
+		data = (data << 8) + tmp;
+}
 
 int	main(void)
 {
+	struct sigaction	sa;
+
+	data = 0;
 	ft_printf("PID: %d\n", getpid());
+	sa.sa_sigaction = &action;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
